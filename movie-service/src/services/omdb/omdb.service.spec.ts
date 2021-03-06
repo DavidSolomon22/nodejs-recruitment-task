76dfaa3949ec6f93dab943507288d70ca549ae42
David Solomon <dav.soli22@gmail.com>
2021-03-06 17:19:13 +0100

    OmdbService (getMovieByTitle) unit tests

--- movie-service/src/services/omdb/omdb.service.spec.ts
+++ movie-service/src/services/omdb/omdb.service.spec.ts
@@ -1,5 +1,10 @@
+import { createMock } from '@golevelup/ts-jest';
 import { HttpService } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { AxiosResponse } from 'axios';
+import { OmdbMovie } from 'common/interfaces';
+import { MovieNotExistException } from 'exceptions/movie-not-exist.exception';
+import { Observable } from 'rxjs';
 import { OmdbService } from '.';
 
 describe('OmdbService', () => {
@@ -13,7 +18,7 @@ describe('OmdbService', () => {
         {
           provide: HttpService,
           useValue: {
-            somefunc: jest.fn(),
+            get: jest.fn(),
           },
         },
       ],
@@ -33,5 +38,57 @@ describe('OmdbService', () => {
 
   it('httpService should be defined', () => {
     expect(httpService).toBeDefined();
+  });
+
+  describe('getMovieByTitle', () => {
+    it('should throw MovieNotExistException when movie not found', async () => {
+      const movieNotFoundResponse = {
+        Response: 'False',
+        Error: 'Movie not found!',
+      };
+      const response = {
+        body: {
+          movieNotFoundResponse,
+        },
+      };
+      jest.spyOn(httpService, 'get').mockReturnValueOnce(
+        createMock<Observable<AxiosResponse<any>>>({
+          toPromise: jest.fn().mockResolvedValueOnce(response),
+        }),
+      );
+      const movieTitle = '12 Angry Men';
+      expect(omdbService.getMovieByTitle(movieTitle)).rejects.toThrowError(
+        MovieNotExistException,
+      );
+    });
+    it('should map omdb response object to OmdbMovie interface and return it', async () => {
+      const omdbResponseObject = {
+        Title: '12 Angry Men',
+        Year: '1957',
+        Rated: 'Approved',
+        Released: '10 Apr 1957',
+        Runtime: '96 min',
+        Genre: 'Crime, Drama',
+        Director: 'Sidney Lumet',
+      };
+      const response = {
+        body: {
+          omdbResponseObject,
+        },
+      };
+      jest.spyOn(httpService, 'get').mockReturnValueOnce(
+        createMock<Observable<AxiosResponse<any>>>({
+          toPromise: jest.fn().mockResolvedValueOnce(response),
+        }),
+      );
+      const movieTitle = '12 Angry Men';
+      const omdbMovie: OmdbMovie = {
+        director: 'Sidney Lumet',
+        genre: 'Crime, Drama',
+        released: '10 Apr 1957',
+      };
+      const res = await omdbService.getMovieByTitle(movieTitle);
+      expect(res).toStrictEqual(omdbMovie);
+    });
   });
 });
